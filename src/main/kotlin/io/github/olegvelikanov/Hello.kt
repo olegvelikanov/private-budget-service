3ba09763a1258ffb32d4ea566a20ded68b641290
Oleg Velikanov <oleg.velikanov@gmail.com>
2019-12-14 18:29:06 +0300

    Initial commit. Let's build something new

---
+++ src/main/kotlin/io/github/olegvelikanov/Hello.kt
@@ -0,0 +1,6 @@
+package io.github.olegvelikanov
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/io/github/olegvelikanov/HelloTest.kt
@@ -0,0 +1,8 @@
+package io.github.olegvelikanov
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
